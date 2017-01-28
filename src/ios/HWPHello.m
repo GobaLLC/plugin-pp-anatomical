cd84ff8d6866ec837bb2d12744cc8f757f33d7ff
Grayson Sharpe <sto2979@gmail.com>
2017-01-28 13:45:54 -0500

    Rearranged a few directories and added background image to resources file

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -4,14 +4,14 @@
 @implementation HWPHello
 
 - (void)greet:(CDVInvokedUrlCommand*)command
-{    
+{
     NSString* jsonDataString = [[command arguments] objectAtIndex:0];
-    
+
     AnatomyViewController *anatomyViewController = [[AnatomyViewController alloc] init];
     anatomyViewController.jsonDataString = jsonDataString;
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:anatomyViewController];
     [self.viewController presentViewController:navController animated:YES completion:nil];
-   
+
     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
